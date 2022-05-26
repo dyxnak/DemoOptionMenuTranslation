fc1ea11309c31b14fd80815a4ef7f881103e6769
21035817 <21035817@myrp.edu.sg>
2022-05-26 21:34:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21035817/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,46 @@
+package sg.edu.rp.c346.id21035817.demooptionmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello ");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            return true;
+        } else {
+            tvTranslatedText.setText("Error translation");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
